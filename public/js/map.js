d4140308b93101857f236bfb913b3e1b91eff6a2
AvatechNG <techissued@gmail.com>
2018-10-18 21:17:33 +0100

    Initial Commit

---
+++ public/js/map.js
@@ -0,0 +1,50 @@
+function initMap(){
+        //map option
+        var options = {
+            zoom : 8,
+            center : {lat:7.377746,lng:3.897250 }
+        }
+        // new map
+        var map = new google.maps.Map(document.getElementById('map'),options);
+        // map marker
+        var marker = new google.maps.Marker({
+            position : {lat:7.377746,lng:3.897250 },
+            map : map,
+            icon : ''
+        })
+        // info window
+        var infoWindow = new google.maps.InfoWindow({
+            content : `<h3>Help needed here</h3>`
+        });
+        // add listener
+        marker.addListener('click',function(){
+            infoWindow.open(map,marker)
+        });
+        // adding marker
+        addMarker({coords :{lat:12.8259,lng: 80.21607 },
+        iconImage : 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'});
+          addMarker({coords :{lat:6.524379,lng: 3.379206 }});
+
+        function addMarker(props){
+
+    var marker = new google.maps.Marker({
+            position : props.coords,
+            map : map,
+            icon : props.iconImage
+        })
+        // info window
+        var infoWindow = new google.maps.InfoWindow({
+            content : `<h3>Help needed here</h3>`
+        });
+        // add listener
+        marker.addListener('click',function(){
+            infoWindow.open(map,marker)
+        });
+        }
+
+    }
+    
+    var width=678;
+    var height=500;
+    self.moveTo((screen.availwidth-width)/2,(screen.availheight-height)/2);
+    self.resizeTo(width,height);
---
+++ resources/views/home.blade.php
@@ -0,0 +1,307 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="wrapper">
+      <div class="container-fluid">
+
+          <div class="row">
+              <div class="col-lg-3">
+                  <div class="card m-b-30">
+                      <div class="card-body">
+
+                        <!-- Messages-->
+                                <!-- item-->
+                                <div class="dropdown-item noti-title">
+                                    <h5><span class="badge badge-danger float-right">9</span>Active Doctor</h5>
+                                </div>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-2.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-2.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-2.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-2.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-2.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-2.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-2.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- All-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    View All
+                                </a>
+
+
+                                <div class="dropdown-item noti-title">
+                                    <h5><span class="badge badge-danger float-right">9</span>Active Patient</h5>
+                                </div>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-3.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-3.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-3.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-3.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-3.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-3.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-3.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- All-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    View All
+                                </a>
+
+
+                                <div class="dropdown-item noti-title">
+                                    <h5><span class="badge badge-danger float-right">9</span>Active Hospitals</h5>
+                                </div>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-4.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-4.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-4.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-4.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-4.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-4.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-4.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- All-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    View All
+                                </a>
+
+
+                                <div class="dropdown-item noti-title">
+                                    <h5><span class="badge badge-danger float-right">9</span>Active Hostpitals</h5>
+                                </div>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-5.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-5.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-5.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-5.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-5.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-5.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <div class="notify-icon"><img src="assets/images/users/avatar-5.jpg" alt="user-img" class="img-fluid rounded-circle" /> Dr. Ibrahim Lukman</div>
+                                </a>
+
+                                <!-- All-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    View All
+                                </a>
+
+                      </div>
+                  </div>
+              </div> <!-- end col -->
+
+              <div class="col-lg-6">
+                  <div class="card m-b-30">
+                      <div class="card-body">
+
+                          <div style="float: right;">
+                              <div id="map" style="width:525px; height:600px;"></div>
+                          </div>
+
+                      </div>
+                  </div>
+              </div> <!-- end col -->
+
+              <div class="col-lg-3">
+                  <div class="card m-b-30">
+                      <div class="card-body" style="margin-left: -34px;">
+
+                            <!-- item-->
+                            <div class="dropdown-item noti-title">
+                                <h5>Incoming Calls (3)</h5>
+                            </div>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item" style="height: 112px !important;">
+                                <div class="notify-icon-right bg-success"><i class="mdi mdi-cart-outline"></i></div>
+                                <p class="notify-details"><b>Your order is placed</b></br><small class="text-muted">Dummy text of the printing an industry.</small></p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item" style="height: 112px !important;">
+                                <div class="notify-icon-right bg-warning"><i class="mdi mdi-message"></i></div>
+                                <p class="notify-details"><b>New Message received</b></br><small class="text-muted">You have 87 unread messages</small></p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item" style="height: 112px !important;">
+                                <div class="notify-icon-right bg-info"><i class="mdi mdi-martini"></i></div>
+                                <p class="notify-details"><b>Your item is shipped</b></br><small class="text-muted">It is a long established fact that a reader will</small></p>
+                            </a>
+
+                            <!-- All-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                View All
+                            </a>
+
+                            <!-- item-->
+                            <div class="dropdown-item noti-title">
+                                <h5>Emergency Request (3)</h5>
+                            </div>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item" style="height: 112px !important;">
+                                <div class="notify-icon-right bg-success"><i class="mdi mdi-cart-outline"></i></div>
+                                <p class="notify-details"><b>Your order is placed</b></br><small class="text-muted">Dummy text of the printing an industry.</small></p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item" style="height: 112px !important;">
+                                <div class="notify-icon-right bg-warning"><i class="mdi mdi-message"></i></div>
+                                <p class="notify-details"><b>New Message received</b></br><small class="text-muted">You have 87 unread messages</small></p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item" style="height: 112px !important;">
+                                <div class="notify-icon-right bg-info"><i class="mdi mdi-martini"></i></div>
+                                <p class="notify-details"><b>Your item is shipped</b></br><small class="text-muted">It is a long established fact that a reader will</small></p>
+                            </a>
+
+                            <!-- All-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                View All
+                            </a>
+
+
+                            <!-- item-->
+                            <div class="dropdown-item noti-title">
+                                <h5>Incoming Calls (3)</h5>
+                            </div>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item" style="height: 112px !important;">
+                                <div class="notify-icon-right bg-success"><i class="mdi mdi-cart-outline"></i></div>
+                                <p class="notify-details"><b>Your order is placed</b></br><small class="text-muted">Dummy text of the printing an industry.</small></p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item" style="height: 112px !important;">
+                                <div class="notify-icon-right bg-warning"><i class="mdi mdi-message"></i></div>
+                                <p class="notify-details"><b>New Message received</b></br><small class="text-muted">You have 87 unread messages</small></p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item" style="height: 112px !important;">
+                                <div class="notify-icon-right bg-info"><i class="mdi mdi-martini"></i></div>
+                                <p class="notify-details"><b>Your item is shipped</b></br><small class="text-muted">It is a long established fact that a reader will</small></p>
+                            </a>
+
+                            <!-- All-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                View All
+                            </a>
+
+                      </div>
+                  </div>
+              </div> <!-- end col -->
+          </div> <!-- end row -->
+
+
+      </div> <!-- end container -->
+  </div>
+  <!-- end wrapper -->
+@endsection
